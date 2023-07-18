2986a1782cc05ef259d8c5ca2bc386420c6b383c
cansley21 <cansley21@gmail.com>
2023-07-18 14:25:07 -0400

    basic tect field/css start

---
+++ zprefix/src/App.js
@@ -0,0 +1,17 @@
+import styles from './App.css';
+
+function App() {
+  return (
+   <div className="App">
+    <div className="information">
+    <input type="text" />
+    <button>Search this Inventory</button>
+    <input type="number"/>
+    <button> Don't Search THIS inventory </button>
+    </div>
+    </div>
+  );
+}
+
+
+export default App;
