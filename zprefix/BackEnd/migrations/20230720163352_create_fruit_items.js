e7a4a66864f99e7ec94abd0fa695705dcd195a52
cansley21 <cansley21@gmail.com>
2023-07-20 12:54:47 -0400

    migrations

---
+++ zprefix/BackEnd/migrations/20230720163352_create_fruit_items.js
@@ -0,0 +1,33 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+  .createTable('user', table => {
+    table.increments('id').primary();
+    table.string('firstname');
+    table.string('lastname');
+    table.string('username');
+    table.string('password');
+  })
+
+  .createTable('items' table => {
+    table.increments('id').primary();
+    table.integer('user_id').unsigned();
+    table.foreign('user_id').references(user.id);
+    table.string('itemname');
+    table.string('description');
+    table.integer('quantity');
+  })
+
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
